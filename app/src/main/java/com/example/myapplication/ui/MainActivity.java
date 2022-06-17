1f08c087bfd85d48601f18cdb7c4feae89847128
Mohmad89 <mohmadadarbeh7@gmail.com>
2022-06-17 11:29:13 +0300

    Lab42

--- app/src/main/java/com/example/myapplication/ui/MainActivity.java
+++ app/src/main/java/com/example/myapplication/ui/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -32,6 +33,21 @@ import com.amplifyframework.datastore.generated.model.Task;
 import com.amplifyframework.datastore.generated.model.Team;
 import com.example.myapplication.R;
 import com.example.myapplication.Adapter.RecyclerViewAdapter;
+import com.google.android.gms.ads.AdError;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.FullScreenContentCallback;
+import com.google.android.gms.ads.LoadAdError;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.OnUserEarnedRewardListener;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.google.android.gms.ads.interstitial.InterstitialAd;
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;
+import com.google.android.gms.ads.rewarded.RewardItem;
+import com.google.android.gms.ads.rewarded.RewardedAd;
+import com.google.android.gms.ads.rewarded.RewardedAdLoadCallback;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -41,9 +57,12 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
 
     private Button mAddTask, mAllTask;
-    private TextView mTextUsername, mTextTeam;
+    private TextView mTextUsername, mTextTeam, mTextScore;
     private RecyclerView mRecyclerView;
     private Handler handler;
+    private FloatingActionButton mInterstitialButton, mRewardedButton;
+    private InterstitialAd mInterstitialAd;
+    private RewardedAd mRewardedAd;
 
     // List<Task> arrayFromRoom;
 
@@ -71,17 +90,48 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Inflate
-        mAddTask = findViewById(R.id.add_task);
-        mAllTask = findViewById(R.id.all_task);
-        mTextUsername = findViewById(R.id.text_username);
-        mRecyclerView = findViewById(R.id.recycler_view);
-        mTextTeam = findViewById(R.id.text_team);
+        inflateView();
 
 
-        recordEvent();
+        // load ad into the AdView
+        AdView mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
+        // load the interstitial ad
+        loadInterstitialAd();
+
+        mInterstitialButton.setOnClickListener(view ->{
+        if (mInterstitialAd != null) {
+            mInterstitialAd.show(MainActivity.this);
+        } else {
+            Log.d(TAG, "The interstitial ad wasn't ready yet.");
+        }
+        });
 
 
-        
+        // load the rewarded ad
+        loadRewardedAd();
+        mRewardedButton.setOnClickListener(view -> {
+            if (mRewardedAd != null) {
+                Activity activityContext = MainActivity.this;
+                mRewardedAd.show(activityContext, new OnUserEarnedRewardListener() {
+
+                    @Override
+                    public void onUserEarnedReward(@NonNull RewardItem rewardItem) {
+                        // Handle the reward.
+                        Log.d(TAG, "The user earned the reward.");
+                        int rewardAmount = rewardItem.getAmount();
+                        String rewardType = rewardItem.getType();
+                        Toast.makeText(activityContext, "the amount => " + rewardAmount, Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+            } else {
+                Log.d(TAG, "The rewarded ad wasn't ready yet.");
+            }
+            mTextScore.setText("5");
+        });
 
 
 //        List<com.example.myapplication.data.Task> arrayList = new ArrayList<>();
@@ -105,11 +155,38 @@ public class MainActivity extends AppCompatActivity {
         mAddTask.setOnClickListener(mAddTaskClick);
         mAllTask.setOnClickListener(mAllTaskClick);
 
+        handler = new Handler(Looper.getMainLooper(), msg -> {
+            mTextScore.setText("5");
+            return true;
+        });
+        mTextTeam.setText(teamTitle);
+
         DataStoreSync();
         // Add Back Button in ActionBar
         ActionBar actionBar = getSupportActionBar();
         // showing the back button in action bar
         actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void inflateView() {
+        mAddTask = findViewById(R.id.add_task);
+        mAllTask = findViewById(R.id.all_task);
+        mTextUsername = findViewById(R.id.text_username);
+        mRecyclerView = findViewById(R.id.recycler_view);
+        mTextTeam = findViewById(R.id.text_team);
+        mInterstitialButton = findViewById(R.id.interstitial_adv_button);
+        mRewardedButton = findViewById(R.id.reward_adv_button);
+        mTextScore = findViewById(R.id.score_text);
+    }
+
+    private void initialisedAdv(){
+        // needs to be initialised once
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+                Log.i(TAG, "onInitializationComplete: AD-MOB INITIALIZED");
+            }
+        });
     }
 
     private void recordEvent() {
@@ -191,6 +268,9 @@ public class MainActivity extends AppCompatActivity {
         } else {
             displayAllTeam(teamTitle);
         }
+
+
+
     }
 
     private void setUsername () {
@@ -289,6 +369,96 @@ public class MainActivity extends AppCompatActivity {
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         //set other important properties
+    }
+
+    // Interstitial Adv
+    private void loadInterstitialAd() {
+        Log.i(TAG, "loadInterstitialAd: ");
+        AdRequest adRequest = new AdRequest.Builder().build();
+
+        InterstitialAd.load(this,"ca-app-pub-3940256099942544/1033173712", adRequest,
+                new InterstitialAdLoadCallback() {
+                    @Override
+                    public void onAdLoaded(@NonNull InterstitialAd interstitialAd) {
+                        // The mInterstitialAd reference will be null until
+                        // an ad is loaded.
+                        MainActivity.this.mInterstitialAd = interstitialAd;
+                        Log.i(TAG, "onAdLoaded");
+
+                        mInterstitialAd.setFullScreenContentCallback(new FullScreenContentCallback(){
+                            @Override
+                            public void onAdDismissedFullScreenContent() {
+                                // Called when fullscreen content is dismissed.
+                                Log.d("TAG", "The ad was dismissed.");
+                            }
+
+                            @Override
+                            public void onAdFailedToShowFullScreenContent(AdError adError) {
+                                // Called when fullscreen content failed to show.
+                                Log.d("TAG", "The ad failed to show.");
+                            }
+
+                            @Override
+                            public void onAdShowedFullScreenContent() {
+                                // Called when fullscreen content is shown.
+                                // Make sure to set your reference to null so you don't
+                                // show it a second time.
+                                mInterstitialAd = null;
+                                Log.d("TAG", "The ad was shown.");
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
+                        // Handle the error
+                        Log.i(TAG, loadAdError.getMessage());
+                        mInterstitialAd = null;
+                    }
+                });
+    }
+
+    // Rewarded Adv
+    private void loadRewardedAd() {
+        AdRequest adRequest = new AdRequest.Builder().build();
+
+        RewardedAd.load(this, "ca-app-pub-3940256099942544/5224354917",
+                adRequest, new RewardedAdLoadCallback() {
+                    @Override
+                    public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
+                        // Handle the error.
+                        Log.d(TAG, loadAdError.getMessage());
+                        mRewardedAd = null;
+                    }
+
+                    @Override
+                    public void onAdLoaded(@NonNull RewardedAd rewardedAd) {
+                        mRewardedAd = rewardedAd;
+                        Log.d(TAG, "Ad was loaded.");
+
+                        mRewardedAd.setFullScreenContentCallback(new FullScreenContentCallback() {
+                            @Override
+                            public void onAdShowedFullScreenContent() {
+                                // Called when ad is shown.
+                                Log.d(TAG, "Ad was shown.");
+                            }
+
+                            @Override
+                            public void onAdFailedToShowFullScreenContent(AdError adError) {
+                                // Called when ad fails to show.
+                                Log.d(TAG, "Ad failed to show.");
+                            }
+
+                            @Override
+                            public void onAdDismissedFullScreenContent() {
+                                // Called when ad is dismissed.
+                                // Set the ad reference to null so you don't show the ad a second time.
+                                Log.d(TAG, "Ad was dismissed.");
+                                mRewardedAd = null;
+                            }
+                        });
+                    }
+                });
     }
 
 
